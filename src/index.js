2d84771db518a787490df1a6919eb0b85edaedf7
Dusan Jonjin <dusan.jonjin@gmail.com>
2020-09-02 23:48:40 +0200

    Implemented ScrollToTop function on page changes.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AmazingShop } from './Components/AmazingShop';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css'
+import ScrollToTop from './Utilities/ScrollToTop';
+import './index.css';
 
 
 ReactDOM.render(
     <React.StrictMode>
         <Router>
+            <ScrollToTop />
             <AmazingShop />
         </Router>
     </React.StrictMode>,
